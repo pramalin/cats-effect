f359ce01bc9b9741923371295bb5bd1f2a54c6e1
Padhu Ramalingam <apadhu@yahoo.com>
2026-02-25 17:01:08 -0500

    Effects exercises completed.

--- src/main/scala/com/rockthejvm/part2effects/Effects.scala
+++ src/main/scala/com/rockthejvm/part2effects/Effects.scala
@@ -2,6 +2,7 @@ package com.rockthejvm.part2effects
 
 import java.time.{Duration, Instant}
 import java.util.{Calendar, Date}
+import scala.io.StdIn
 
 object Effects {
 
@@ -23,26 +24,63 @@ object Effects {
    *  Exercises
    *  1. An IO which returns current time of the System
    *  2. An IO which measures the duration of a computation (hint: use ex 1)
+   *  3. An IO which prints something to the console
+   *  4. An IO something reads from console
    */
 
   // 1
-  def currentTime(): MyIO[Instant] =
-     MyIO(() => {
-       Instant.now()
-    })
-
+  def currentTime(): MyIO[Long] = MyIO(() => System.currentTimeMillis())
   // 2
-  def measure[A](computation: MyIO[A]): MyIO[Long] =
+  def mymeasure[A](computation: MyIO[A]): MyIO[Long] =
     MyIO(() => {
       val startTime = currentTime().unsafeRun()
       computation.unsafeRun()
       val endTime = currentTime().unsafeRun()
 
-      Duration.between(startTime, endTime).toMillis();
+      startTime - endTime
   })
 
+  // answers:
+  val clock: MyIO[Long] = MyIO(() => System.currentTimeMillis())
+  def measure[A](computation: MyIO[A]): MyIO[Long] = for {
+      startTime <- clock
+      _ <- computation
+      endTime <- clock
+    } yield endTime - startTime
+/*
+  clock.flatMap(startTime => computation.flatMap(_ => clock.map(endTime => endTime - startTime)))
+
+  clock.map(endTime => endTime - startTime) = MyIO(() => System.currentTimeMillis() - startTime)
+  clock.flatMap(startTime => computation.flatMap(_ => MyIO(() => System.currentTimeMillis() - startTime)))
+
+ */
+
+  def testTimeIO(): Unit = {
+    val test = measure(MyIO(() => Thread.sleep(1000)))
+    println(test.unsafeRun())
+  }
+
+  // 3
+  def putStrLn(text: String): MyIO[Unit] = MyIO(() => println(text))
+
+  // 4
+  val read: MyIO[String] = MyIO(() => StdIn.readLine())
+
+  def testConsole(): Unit = {
+    val program: MyIO[Unit] = for {
+      line1 <- read
+      line2 <- read
+      _ <- putStrLn(line1 + line2)
+    } yield ()
+
+    program.unsafeRun()
+  }
+
   def main(args: Array[String]): Unit = {
-    val runTime = measure(anIO)
-   println(s"runTime: ${runTime.unsafeRun()}")
+//    val runTime = mymeasure(anIO)
+//    val runTime = measure(anIO)
+//    println(s"runTime: ${runTime.unsafeRun()}")
+//      testTimeIO();
+    testConsole()
   }
 }
