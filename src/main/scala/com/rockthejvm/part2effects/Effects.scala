288e67be3e9c4a5d8adb91aec49c89f7f62523d6
Padhu Ramalingam <apadhu@yahoo.com>
2026-02-25 12:43:38 -0500

    my answer to Effects exercises 1 and 2

--- src/main/scala/com/rockthejvm/part2effects/Effects.scala
+++ src/main/scala/com/rockthejvm/part2effects/Effects.scala
@@ -1,5 +1,8 @@
 package com.rockthejvm.part2effects
 
+import java.time.{Duration, Instant}
+import java.util.{Calendar, Date}
+
 object Effects {
 
 
@@ -12,10 +15,34 @@ object Effects {
   }
 
   val anIO: MyIO[Int] = MyIO(() => {
-    println("I'm writting something...")
+    println("I'm writing something...")
     42
   })
+
+  /**
+   *  Exercises
+   *  1. An IO which returns current time of the System
+   *  2. An IO which measures the duration of a computation (hint: use ex 1)
+   */
+
+  // 1
+  def currentTime(): MyIO[Instant] =
+     MyIO(() => {
+       Instant.now()
+    })
+
+  // 2
+  def measure[A](computation: MyIO[A]): MyIO[Long] =
+    MyIO(() => {
+      val startTime = currentTime().unsafeRun()
+      computation.unsafeRun()
+      val endTime = currentTime().unsafeRun()
+
+      Duration.between(startTime, endTime).toMillis();
+  })
+
   def main(args: Array[String]): Unit = {
-   println(s"io: ${anIO.unsafeRun()}")
+    val runTime = measure(anIO)
+   println(s"runTime: ${runTime.unsafeRun()}")
   }
 }
