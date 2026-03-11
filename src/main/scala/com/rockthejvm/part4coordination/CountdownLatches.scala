51104e7e6710050e968f010a244e58e9740bc296
Padhu Ramalingam <apadhu@yahoo.com>
2026-03-11 11:51:39 -0400

    added CountdownLatches.

---
+++ src/main/scala/com/rockthejvm/part4coordination/CountdownLatches.scala
@@ -0,0 +1,105 @@
+package com.rockthejvm.part4coordination
+
+import cats.effect.std.CountDownLatch
+import cats.effect.{IO, IOApp, Resource}
+
+import scala.concurrent.duration.*
+import com.rockthejvm.utils.*
+import cats.syntax.parallel.*
+
+import java.io.FileWriter
+import java.io.File
+import scala.io.Source
+
+object CountdownLatches extends IOApp.Simple {
+
+  /*
+    CDLatches are coordination primitive initialized with a count.
+    All fibers calling await() on the CDLatch are (semantically) blocked.
+    When the internal count of the latch reaches 0 (via release() calls from other fibers), all the awaiting fibers are unblocked.
+   */
+
+  def announcer(latch: CountDownLatch[IO]): IO[Unit] = for {
+    _ <- IO("Starting race shortly...").debug >> IO.sleep(2.seconds)
+    _ <- IO("5...").debug >> IO.sleep(1.second)
+    _ <- latch.release
+    _ <- IO("4...").debug >> IO.sleep(1.second)
+    _ <- latch.release
+    _ <- IO("3...").debug >> IO.sleep(1.second)
+    _ <- latch.release
+    _ <- IO("2...").debug >> IO.sleep(1.second)
+    _ <- latch.release
+    _ <- IO("1...").debug >> IO.sleep(1.second)
+    _ <- latch.release
+    _ <- IO("GO GO GO!").debug
+  } yield ()
+
+  def createRunner(id: Int, latch: CountDownLatch[IO]): IO[Unit] = for {
+    _ <- IO(s"[runner $id] waiting for signal...").debug
+    _ <- latch.await // block this fiber until the count reaches 0
+    _ <- IO(s"[runner $id] RUNNING!").debug
+  }  yield ()
+
+  def sprint(): IO[Unit] = for {
+    latch <- CountDownLatch[IO](5)
+    announcerFib <- announcer(latch).start
+    _ <- (1 to 10).toList.parTraverse(id => createRunner(id, latch))
+    _ <- announcerFib.join
+  } yield ()
+
+  /**
+   * Exercise: simulate a file downloader on multiple threads
+   */
+  object FileServer {
+    val fileChunksList = Array(
+      "I love Scala,",
+      "Cats Effect seems quite fun.",
+      "Never would I have thought I would do low-level concurrency WITH pure FP."
+    )
+
+    def getNumChunks: IO[Int] = IO(fileChunksList.length)
+    def getFileChunk(n: Int): IO[String] = IO(fileChunksList(n))
+  }
+
+  def writeToFile(path: String, contents: String): IO[Unit] = {
+    val fileResource = Resource.make(IO(new FileWriter(new File(path))))(writer => IO(writer.close()))
+      fileResource.use { writer =>
+        IO(writer.write(contents))
+    }
+  }
+
+  def appendFileContents(fromPath: String, toPath: String): IO[Unit] = {
+    val compositeResource = for {
+      reader <- Resource.make(IO(Source.fromFile(fromPath)))(source => IO(source.close()))
+      writer <- Resource.make(IO(new FileWriter(new File(toPath), true)))(writer => IO(writer.close()))
+    } yield (reader, writer)
+
+    compositeResource.use {
+      case (reader, writer) => IO(reader.getLines().foreach(writer.write))
+    }
+  }
+
+  /*
+    - call file server API and get the number of chunks (n)
+    - start a CDLatch
+    - start n fibers which download a chunk of the file (use the file server's download chunk API)
+    - block on the latch until each task has finished
+    - after all chunks are done, stitch the files together under the same file on disk
+   */
+/* my attempt
+  def downloadFile(filename: String, destFolder: String): IO[Unit] = for {
+    numChunks <- FileServer.getNumChunks
+    latch <- CountDownLatch[IO](numChunks)
+    contents <- (1 to numChunks).toList.parTraverse{n =>
+      for {
+        chunk <- FileServer.getFileChunk(n)
+        _ <- latch.release
+      } yield (chunk)
+    }
+    _ <- latch.await
+    tmpFile <- appendFileContents("tmp-file", contents) ???
+    _ <- writeToFile(s"$destFolder/$filename", tmpFile) ???
+  } yield ()
+*/
+  override def run: IO[Unit] = sprint()
+}
